0b92b89c652ce81a8a4c57e5d33269a63ced121f
张江 <896884553@qq.com>
2018-06-13 23:48:03 +0800

    添加client说明

--- 018-网页特效2/006-commentTool.js
+++ 018-网页特效2/006-commentTool.js
@@ -2,6 +2,7 @@ function $(id) {
     return document.getElementById(id);
 }
 
+/*返回兼容的scrollTop和scrollLeft*/
 function scroll() {
     if (window.pageXOffset != null){
         //ie9和其他浏览器
@@ -20,6 +21,30 @@ function scroll() {
         return {
             left:document.body.scrollLeft,
             top:document.body.scrollTop
+        }
+    }
+}
+
+
+/*检测可是区域的屏幕宽度和高度*/
+function client() {
+    if (window.pageXOffset != null){
+        //ie9和其他浏览器
+        return {
+            width:window.innerWidth,
+            height:window.innerHeight
+        }
+    } else if (document.compatMode == 'CSS1Compat'){
+        //检测是不是怪异模式的浏览器，就是没有申明<!DOCTYPE html>
+        return {
+            width:window.documentElement.clientWidth,
+            height:window.documentElement.clientHeight
+        }
+    }else {
+        //剩下是怪异浏览器模式
+        return {
+            width:document.body.clientWidth,
+            height:document.body.clientHeight
         }
     }
 }
