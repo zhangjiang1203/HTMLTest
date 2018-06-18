d85a17398a59d523a3af1fc59d1a9a5f97cadd62
张江 <896884553@qq.com>
2018-06-18 23:21:01 +0800

    添加手风琴效果

--- 020-网页特效4/commentTool.js
+++ 020-网页特效4/commentTool.js
@@ -60,7 +60,7 @@ function getStyle(obj,attr) {
 /*设置多个属性的动画，谁执行动画 json传入动画属性和目标值 fn函数体是
  * 动画执行完之后要执行的函数
  * fn的值可以为空*/
-function  customBackAnimate(obj,json,fn) {
+function  customBackAnimate(obj,json,time=5,fn) {
     //清除定时器
     clearInterval(obj.timer);
     obj.timer = setInterval(function () {
@@ -68,10 +68,29 @@ function  customBackAnimate(obj,json,fn) {
         var flag = true;
         //获得属性值
         for (var attr in json){
-            var current = parseInt(getStyle(obj,attr));
+            var current = 0;
+            if (attr == 'opacity'){
+                current = Math.round(parseInt(getStyle(obj,attr))*100) || 0;
+                console.log(current);
+            } else {
+                current = parseInt(getStyle(obj,attr));//获取到数值
+            }
             var step = (json[attr] - current)/10;
             step =  step > 0 ? Math.ceil(step) : Math.floor(step);
-            obj.style[attr] = current + step + 'px';
+            //添加透明度判断
+            if (attr == 'opacity'){
+                //判断用户有没有输入opacity
+                if ('opacity' in obj.style){
+                    //判断浏览器是否支持opacity这个属性
+                    obj.style[attr] = (current + step) / 100;
+                }else {
+                    obj.style.filter = 'alpha(opacity=' + (current + step) + ')';
+                }
+            }else if (attr == 'zIndex'){
+                obj.style[attr] = json[attr];
+            } else {
+                obj.style[attr] = current + step + 'px';
+            }
 
             console.log(current);
             if (current != json[attr]){
@@ -87,5 +106,5 @@ function  customBackAnimate(obj,json,fn) {
                 fn();//如果定义了fn就执行这个函数
             }
         }
-    },30)
+    },time)
 }
