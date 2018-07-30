7e5cbb55e03c820180814f0c62557dfe867c2213
zhangjiang1203 <896884553@qq.com>
2018-07-30 14:00:41 +0800

    修改第三屏和第四屏的动画效果

--- 08-h5css3/01-购物车宣传页/js/index.js
+++ 08-h5css3/01-购物车宣传页/js/index.js
@@ -11,7 +11,25 @@ $(function () {
         afterLoad:function (link,index) {
         //    监听进入某一屏的回调方法
             $('.section').eq(index-1).addClass('now');
-            
+        },
+        //组件初始化完毕
+        afterRender:function() {
+            //切换到下一页
+            //jquery对象的封装，$.fn.background = function(){}
+            //jquery本身没有方法通过$.fn的方式追加上去，认为是插件方法
+            $('#movedown').on('click',function () {
+                console.log('开始点击');
+                // e.stopPropagation();
+                $.fn.fullpage.moveSectionDown();
+            });
+        },
+        //离开某个页面触发的函数
+        //有三个参数 index nextIndex direction  direction的值为up或者down
+        onLeave:function (index,nextIndex,direction) {
+            // if (index == 2 && nextIndex == 3){
+            //    第二页到第三页
+                $('.section').eq(index-1).addClass('leaved');
+            // }
         }
     });
 })
