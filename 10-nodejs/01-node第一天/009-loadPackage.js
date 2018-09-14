67af8b4932effc36cf247409766477a56adfc032
zhangjiang <zhangjiang@imcoming.cn>
2018-09-14 10:33:56 +0800

    添加移动web学习笔记和项目工程

--- 10-nodejs/01-node第一天/009-loadPackage.js
+++ 10-nodejs/01-node第一天/009-loadPackage.js
@@ -6,3 +6,4 @@ exports.foo = 'hello'
 exports.add = function (x,y) {
     return x + y;
 }
+
--- 10-nodejs/07-node第七天/01-文件路径.js
+++ 10-nodejs/07-node第七天/01-文件路径.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path')
 
 //拼接为一个绝对路径
-// __dirname和————filename就是专门用来获取当前文件以及文件所属目录的绝对路径
+// __dirname和__filename就是专门用来获取当前文件以及文件所属目录的绝对路径
 var filePath = path.join(__dirname, './template.txt')
 
 fs.readFile(filePath,function (err,data) {
